58c0c7d84d49f5c23e799364b6d66e5b07286e15
Benjamin <rose.benjamin925@gmail.com>
2018-04-07 13:05:34 -0700

    gloves off

---
+++ assets/javascript/logic.js
@@ -0,0 +1,13 @@
+function firebaseInit() {
+  // Initialize Firebase
+  firebase.initializeApp({
+    apiKey: "AIzaSyAP426TUbMdQpuzfglVFpU00R2n4-M32IE",
+    authDomain: "bcb-project-1-cd6d1.firebaseapp.com",
+    databaseURL: "https://bcb-project-1-cd6d1.firebaseio.com",
+    projectId: "bcb-project-1-cd6d1",
+    storageBucket: "bcb-project-1-cd6d1.appspot.com",
+    messagingSenderId: "561097617085"
+  });
+
+  window.db = firebase.database()
+}

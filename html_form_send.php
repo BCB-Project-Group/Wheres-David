e8c6f51ec45776bc9027865a814c8f871b84eb99
yaliu8 <yaliu8@gmail.com>
2018-04-15 16:53:33 -0700

    email test

--- html_form_send.php
+++ html_form_send.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
 
     // CHANGE THE TWO LINES BELOW
-    $email_to = "yaliu8@gmail.com ";
+    $email_to = "yaliu8@gmail.com";
 
     $email_subject = "Message from Contact";
 

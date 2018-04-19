81980cb8e967403e9dba3b0ffa65a309efca058f
Benjamin <rose.benjamin925@gmail.com>
2018-04-19 00:48:02 -0700

    map is responsive

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -18,13 +18,18 @@ $(document).ready(function () {
 function viewPortScale() {
   //set wrapper height based off of viewport
 
-  let width = $(window).width;
-  // if (width() > 765) {
 
 
-    $("#wrapper").height(
-      $(window).height() - $("header").height() + 10);
-  // }
+  $("#wrapper").height(
+    $(window).height() - $("header").height() + 10);
+
+  if ($(window).width() < 768) {
+    $("#map-modal").height($(window).height() - 70);
+  }
+
+  if ($(window).width() < 600) {
+    $("#map-modal").height($(window).height() -50)
+  }
 
 
 }
@@ -457,7 +462,7 @@ function createCommon() {
 
     resize: () => {
 
-      $(window).on("resize", () => {
+      $(window, "#wrapper").on("resize", () => {
         viewPortScale()
       });
     },
